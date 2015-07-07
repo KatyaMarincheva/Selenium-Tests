706b093f6a61b3ab266b281e1a31bcda3e7cc008
Katya Marincheva <katya.marincheva@outlook.com>
2015-07-07 15:51:15 +0300

    Selenium Tests C#

---
+++ WebDriverDemoTests/MainProgram/MainProgram.cs
@@ -0,0 +1,14 @@
+﻿namespace MainProgram
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+
+    class MainProgram
+    {
+        static void Main()
+        {
+            IWebDriver wdriver = new ChromeDriver(@"dependencies");
+            wdriver.Navigate().GoToUrl("https://softuni.bg/trainings/1175/High-Quality-Code-July-2015");
+        }
+    }
+}
---
+++ WebDriverDemoTests/WebDriverLoginTest/WebDriverLogin.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebDriverLoginTest
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Security.Principal;
+    using OpenQA.Selenium.Support.UI;
+
+    [TestClass]
+    public class WebDriverLogin
+    {
+        [TestMethod]
+        public void RedirectToLoginFromHomePage()
+        {
+            using (IWebDriver wdriver = new ChromeDriver(@"dependencies"))
+            {
+                wdriver.Navigate().GoToUrl("https://softuni.bg");
+                wdriver.Manage().Window.Maximize();
+                wdriver.FindElement(By.Id("loginLink")).Click();
+
+                Assert.AreEqual(wdriver.Url, "https://softuni.bg/account/authenticate");
+                wdriver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void FindLinkByLinkTextTest()
+        {
+            using (IWebDriver wdriver = new ChromeDriver(@"dependencies"))
+            {
+                wdriver.Navigate().GoToUrl("https://softuni.bg/trainings/1175/High-Quality-Code-July-2015");
+                IWebElement firstLink = wdriver.FindElement(By.LinkText("Предишни инстанции на курса"));
+
+                Assert.AreEqual("https://softuni.bg/courses/high-quality-code/", firstLink.GetAttribute("href"));
+                wdriver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void TestTable()
+        {
+            using (IWebDriver wdriver = new ChromeDriver(@"dependencies"))
+            {
+                wdriver.Navigate().GoToUrl("https://softuni.bg/trainings/1175/High-Quality-Code-July-2015");
+                IWebElement simpleTable = wdriver.FindElement(By.Id("lectures-table"));
+
+                //Get all rows
+                IList rows = simpleTable.FindElements(By.TagName("tr"));
+                Assert.AreEqual(30, rows.Count);
+                wdriver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void FindEelementByCSSTypeTest()
+        {
+            using (IWebDriver wdriver = new ChromeDriver(@"dependencies"))
+            {
+                wdriver.Navigate().GoToUrl("https://softuni.bg/account/authenticate");
+                wdriver.FindElement(By.LinkText("Кандидатствай")).Click();
+
+                Assert.AreEqual(wdriver.Url, "https://softuni.bg/account/authenticate?returnUrl=%2Fusers%2Fcandidate%2Fregister");
+                wdriver.Quit();
+            }
+        }
+    }
+}
